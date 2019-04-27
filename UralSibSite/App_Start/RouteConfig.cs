dc714f4b22a5fa659068c2df9734a1afc957af09
Евгений Плющ <edplyusch@edu.hse.ru>
2019-04-27 12:22:11 +0300

    started

--- UralSibSite/App_Start/RouteConfig.cs
+++ UralSibSite/App_Start/RouteConfig.cs
@@ -16,8 +16,13 @@ namespace UralSibSite
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Main", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Home", action = "Main", id = UrlParameter.Optional }
+        );
         }
     }
 }
